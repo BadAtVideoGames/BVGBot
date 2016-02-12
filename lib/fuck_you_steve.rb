a55afdb2c1889d9ebc35754293d6fcc14b8944df
John Epperson <j.epperson@verizon.net>
2016-02-12 00:44:07 -0500

    Improve Fuck You Steve Regex

--- lib/fuck_you_steve.rb
+++ lib/fuck_you_steve.rb
@@ -3,13 +3,33 @@ module BVG
     def self.included(base)
       base.extend(ClassMethods)
       base.class_eval do
-        register_class_event(Discordrb::Events::MessageEvent, containing: /(f|F)(uck)? you.*?(s|S)teve/, &method(:reply_with_fuck_you_too_q))
+        register_class_event(Discordrb::Events::MessageEvent, containing: fuck_you_steve_regex, &method(:reply_with_fuck_you_too_q))
       end
     end
 
     module ClassMethods
       def reply_with_fuck_you_too_q(event)
         event.respond "#{event.user.mention} Steve says, 'Fuck you too Q!'"
+      end
+
+      def fuck_you_steve_regex
+        /\b(#{curse_words})\b#{space_and_punctuation}*?\b(#{you_words})\b#{space_and_punctuation}*?\b(#{steve_names})\b/i
+      end
+
+      def curse_words
+        %w[fuck fuq f fudge fug].join("|")
+      end
+
+      def you_words
+        %w[you u yu yoo].join("|")
+      end
+
+      def steve_names
+        %w[steve steeve steev stev ste stevia].join("|")
+      end
+
+      def space_and_punctuation
+        "[\\W_-]"
       end
     end
   end
