cfcdac251f83bd5423a390a12b71968c46ea02a0
John Epperson <j.epperson@verizon.net>
2016-02-12 13:35:46 -0500

    Add more randomization as well as a high image count to bot

--- lib/google_images.rb
+++ lib/google_images.rb
@@ -20,18 +20,32 @@ module BVG
       end
 
       def reply_with_image(event)
-        respond_with_image(event, images(image_query(event.message.content)))
+        respond_with_images(event)
       end
 
       def reply_with_unsafe_image(event)
-        respond_with_image(event, images(image_query(event.message.content, false), false))
+        respond_with_images(event, false)
       end
 
-      def respond_with_image(event, response_hash)
-        if response_hash['searchInformation']['totalResults'].to_i > 0
-          image = response_hash['items'][rand(response_hash['items'].count)]
+      def respond_with_images(event, safe = true)
+        results = images(event, {}, safe)
+        images = []
+
+        results_indexes(event, [results['searchInformation']['totalResults'].to_i, 100].max).each do |index|
+          if results['queries']['request'].first['startIndex'] == (index / 10 * 10 + 1)
+            images << results['items'][index % 10 - 1]
+          else
+            results = images(event, { startIndex: (index / 10 * 10 + 1) }, safe)
+            images << results['items'][index % 10 - 1]
+          end
+        end
+        images.each do |image|
           event.respond "#{image['link']}"
         end
+      end
+
+      def results_indexes(event, max_index)
+        (number_of_images(event.message.content) || 1).to_i.times.map{|n| rand(max_index)}.uniq.sort
       end
 
       def google_images_uri
@@ -39,26 +53,38 @@ module BVG
       end
 
       def image_query(message, safe = true)
+        safe ? image_regexp.match(message)[3] : unsafe_image_regexp.match(message)[3]
+      rescue
+        nil
+      end
+
+      def number_of_images(message, safe = true)
         safe ? image_regexp.match(message)[2] : unsafe_image_regexp.match(message)[2]
       rescue
         nil
       end
 
       def image_regexp
-        /\\(image|img) (.*)/i
+        /\\(image|img)( \d+)? (.*)/i
       end
 
       def unsafe_image_regexp
-        /\\(unsafe_image) (.*)/i
+        /\\(unsafe_image)( \d+)? (.*)/i
       end
 
-      def images(query, safe = true)
+      def images(event, options = {}, safe = true)
         response = RestClient.get(google_images_uri.to_s, params: { key: @@google_api_key, cx: @@google_custom_search_id,
-          q: query, safe: safe ? 'medium' : 'off', searchType: 'image' })
+          q: image_query(event.message.content, safe), safe: safe ? 'medium' : 'off', searchType: 'image', count: 100 }.merge(options))
 
         JSON.parse(response)
       rescue
         []
+      end
+
+      def total_images_in_search(query, safe = true)
+        images(query, safe)['searchInformation']['totalResults']
+      rescue
+        0
       end
     end
   end
